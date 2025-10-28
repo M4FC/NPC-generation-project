4e240337251877bf27ddda20d8a0b2685293a0f6
m4fc <mateovasquezus@gmail.com>
2025-10-28 18:23:49 +0000

    did a lil

---
+++ npc_gen.py
@@ -0,0 +1,17 @@
+import random
+if 1==1:
+ start=""
+ while start!= "yes":
+    start=input("ready to start the generator? Type yes to start.")
+
+malenames=["Geraldine,Peter,Derrick,Eugene,Joseph"]
+femalenames=["Hailey,Madison,Irene,Cassidy,Lexi"]
+male=malenames
+female=femalenames
+genderlist=[male,female]
+gender=random.choice(genderlist)
+
+
+
+     
+ 
