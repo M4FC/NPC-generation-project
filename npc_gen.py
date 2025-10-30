e4e66b181272989726cf635dec3ff516efeb3527
m4fc <mateovasquezus@gmail.com>
2025-10-30 16:26:54 +0000

    more did things

--- npc_gen.py
+++ npc_gen.py
@@ -4,12 +4,31 @@ if 1==1:
  while start!= "yes":
     start=input("ready to start the generator? Type yes to start.")
 
-malenames=["Geraldine,Peter,Derrick,Eugene,Joseph"]
-femalenames=["Hailey,Madison,Irene,Cassidy,Lexi"]
-male=malenames
-female=femalenames
+malenames=["Geraldine","Peter","Derrick","Eugene","Joseph"]
+femalenames=["Hailey","Madison","Irene","Cassidy","Lexi"]
+male=(random.choice(malenames))
+female=(random.choice(femalenames))
 genderlist=[male,female]
 gender=random.choice(genderlist)
+if gender==male:
+  charnames=(male)
+elif gender==female:
+  charnames=(female)
+atcharnames=charnames
+Heights=["1.1","1,2","1.3","1.4","1.5","1.7","1.8","2.0","2.5"]
+atheight=(random.choice(Heights))
+strength=[10,20,30,40,50,60,70,80,90,100]
+strongboi=(random.choice(strength))
+print(strongboi)
+for i in strength:
+  print(strongboi)
+
+
+
+  
+
+
+
 
 
 
